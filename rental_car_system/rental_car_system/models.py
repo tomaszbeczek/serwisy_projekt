6ae225a006c411632e974d0bed571d0b3407b942
Damian Dybciak <damian.dybciak00@gmail.com>
2022-01-18 20:26:16 +0100

    change model Car and add model Location

--- rental_car_system/rental_car_system/models.py
+++ rental_car_system/rental_car_system/models.py
@@ -5,5 +5,19 @@ from django.db import models
 class Car(models.Model):
     vehicle_brand = models.CharField(max_length=20)
     vehicle_model = models.CharField(max_length=13)
+    max_tank = models.IntegerField(max_length=2)
+    available = models.BooleanField(default=True)
+    location = models.ForeignKey(Location, on_delete = models.SET_NULL,null = True)
+    rent = models.CharField(max_length=10, blank = True)
+
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.city
+
+
 
 
